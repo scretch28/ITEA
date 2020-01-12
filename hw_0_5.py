ab14cc1cc1334299ccf576c56f0c398c54f05f22
nia <nidzik@gmail.com>
2020-01-12 23:00:55 +0200

    площа прямокутника 1

---
+++ hw_0_5.py
@@ -0,0 +1,10 @@
+#знаходимо площу прямокутника S=ab
+a=int(input("enter the number a: "))
+print(a)
+b=int(input('enter the number b: '))
+print(b)
+s=a*b
+#print('Площа прямокутника s=',s)
+print('Площа прямокутника s= %s' % s)
+
+
