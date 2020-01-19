c1eff0ae40772e6dafb263b01ba16ea96625207f
nia <nidzik@gmail.com>
2020-01-19 19:45:59 +0200

    +1_1_2

--- hw_1_1_2.py
+++ hw_1_1_2.py
@@ -5,11 +5,22 @@ Created on Thu Jan 16 23:48:59 2020
 @author: nia
 """
 
-def getDate():
-    day=int(input('Enter the day: '))
-    month=int(input('Enter the month: '))
-    year=int(input('Enter the year: '))
-    return (day,month,year)
+def get_dates():
+    day1=int(input('Enter the day  of birsday: '))
+    month1=int(input('Enter the month of birsday: '))
+    year1=int(input('Enter the year of birsday: '))
+    day2=int(input('Enter the current day: '))
+    month2=int(input('Enter the current month: '))
+    year2=int(input('Enter the current year: '))
+    return {'day1':day1,'month1':month1,'year1':year1,'day2':day2,'month2':month2,'year2':year2}
+    
+def count_dates(dates):
+    numdays=(dates['year2']-dates['year1'])*360+(dates['month2']-dates['month1'])*30+ (dates['day2']-dates['day1'])  #з огляду на умови завдання, 12міс*30=360 днів
+    if dates['day2']<dates['day1']:
+        numdays+=1
+    nummonths=numdays//30
+    numyears=nummonths//12
+    return{'days':numdays,'month':nummonths,'years': numyears}
 
 
 #1 запрос імені та прізвища користувача:
@@ -19,9 +30,23 @@ name=input('Enter first name and last name: ')
 print('Hello!', name)
 
 #запит дня дати народження:
-print('Enter your birsday')
-dateB=getDate()
+dates=get_dates()
+q=count_dates(dates)
+#print('days: ', q['days'])
+print('month: ', q['month'])
+print('years: ', q['years'])
 
-print('Enter current day: ')
-dateL=getDate()
+dates['day1']=10
+dates['month1']=1
+dates['year1']=2020
+
+q=count_dates(dates)
+print('days: ', q['days'])
+print('month: ', q['month'])
+print('years: ', q['years'])
+
+
+
+
+
 
